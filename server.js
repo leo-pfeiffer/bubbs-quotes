b62d74b1ed3a2ff05093187d3c5711dfc762ec02
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2021-02-19 09:58:04 +0000

    Modified port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require('./index')
 
-app.listen(3000, (err) => {
+let server = app.listen(process.env.PORT || 8080, (err) => {
+    let port = server.address().port;
     if (err) throw err
-    console.log('Server running on http://127.0.0.1:3000')
+    console.log('Server running on port', port)
 })
