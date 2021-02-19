d82854663827a2eb1f126763266e080bf37065aa
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2021-02-19 14:37:12 +0000

    Added CORS header

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const menu = require('./routes/menu')
 const quote = require('./routes/quote')
+const cors = require('cors')
 
 const app = express()
-
+app.use(cors())
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use('/menu', menu)
