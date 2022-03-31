5f160c0bd39f6c48006f265733072ed257d2bbd0
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-03-31 07:39:55 +0100

    Remove demo service

--- routes/menu.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const menuService = require('../services/menu.service')
-
-router.get('/', menuService.getMenu)
-
-module.exports = router
