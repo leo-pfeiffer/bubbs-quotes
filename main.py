8f37c94e29164299cc3cb58f4c8135b4df4db34a
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2022-04-30 17:08:59 +0100

    add CORS middleware

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from dao import dao
 from pydantic import BaseModel
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount("/static", StaticFiles(directory="static", html=True), name="static")
 
